4ee72310dcfcd607f2cdc9904f143f60976dbb24
gabriel gomes <gabrielluizgomes1@gmail.com>
2024-01-11 17:43:55 -0300

    criando layout do jogo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 
-background_colour = (255,255,255)
-(width, height) = (300, 200)
+main_background_colour = (19, 1, 57)
+secondary_background_colour = (15, 0, 46)
+(width, height) = (946, 500)
 
 screen = pygame.display.set_mode((width, height))
 
@@ -10,7 +11,10 @@ pygame.display.set_caption('Duck Stock')
 Icon = pygame.image.load('assets/duck.svg')
 pygame.display.set_icon(Icon)
 
-screen.fill(background_colour)
+screen.fill(main_background_colour)
+
+pygame.draw.rect(screen, secondary_background_colour, pygame.Rect(0, 0, 153, height))
+pygame.draw.rect(screen, secondary_background_colour, pygame.Rect(700, 0, width - 700, height))
 
 pygame.display.flip()
 
